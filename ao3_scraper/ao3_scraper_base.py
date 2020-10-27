0ae1715502c49be3364de80922bebc0f2252353f
Dashiell Stander <dash.stander@gmail.com>
2020-10-26 19:03:52 -0700

    Begin to refactor

--- ao3_scraper/ao3_scraper_base.py
+++ ao3_scraper/ao3_scraper_base.py
@@ -6,6 +6,8 @@ user_agent = 'Chrome/52 (Macintosh; Intel Mac OS X 10_10_5); Dashiell Stander/UC
 
 class Ao3ScraperBase:
     base_url = 'https://www.archiveofourown.com'
+    url_prefix = ''
+    url_postfix = ''
 
     def __init__(self):
         self.user_agent = user_agent
@@ -15,11 +17,14 @@ class Ao3ScraperBase:
     def _make_headers(self):
         return {'user-agent': self.user_agent}
 
+    def _make_url(self, category):
+        return f'{self.url_prefix}/{category}/{self.url_postfix}'
+
     def get(self, url):
         return requests.get(f'{self.base_url}/{url}', headers=self.headers)
 
     def load(self, request):
         return BeautifulSoup(request.text, 'lxml')
     
-    def scrape(self, page):
+    def scrape(self):
         raise NotImplementedError
---
+++ ao3_scraper/work_id_scraper.py
@@ -0,0 +1,8 @@
+from ao3_scraper.ao3_scraper_base import Ao3ScraperBase
+
+
+class WorkIdScraper(Ao3ScraperBase):
+
+    def __init__(self):
+        super().__init__()
+
